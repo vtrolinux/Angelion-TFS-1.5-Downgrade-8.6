78ba14ad593bbb8a485a7cabb2f8313b74d01b36
Breno Alves <contatobrenoalves@gmail.com>
2022-11-27 21:04:08 -0300

    Hide magic effect on no tile zone

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -1678,6 +1678,11 @@ void ProtocolGame::sendMagicEffect(const Position& pos, uint8_t type)
 		return;
 	}
 
+	Tile* tile = g_game.map.getTile(pos);
+	if (!tile || !tile->getGround()) {
+		return;
+	}
+
 	NetworkMessage msg;
 	msg.addByte(0x83);
 	msg.addPosition(pos);
